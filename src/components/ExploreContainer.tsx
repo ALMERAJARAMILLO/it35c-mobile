85fd1f8daedd685f0d63e4e5a51e8c79a16cbbad
ALMERAJARAMILLO <jaramilloalmera92@gmail.com>
2026-03-26 15:45:21 +0800

    Added components

---
+++ src/components/ExploreContainer.tsx
@@ -0,0 +1,37 @@
+import './ExploreContainer.css';
+import { IonBadge, IonItem, IonLabel, IonList } from '@ionic/react';
+
+interface ContainerProps { }
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <IonList>
+      <IonItem>
+        <IonLabel>Followers</IonLabel>
+        <IonBadge color="primary">22k</IonBadge>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Likes</IonLabel>
+        <IonBadge color="secondary">118k</IonBadge>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Stars</IonLabel>
+        <IonBadge color="tertiary">34k</IonBadge>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Completed</IonLabel>
+        <IonBadge color="success">80</IonBadge>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Warnings</IonLabel>
+        <IonBadge color="warning">70</IonBadge>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Notifications</IonLabel>
+        <IonBadge color="danger">1000</IonBadge>
+      </IonItem>
+    </IonList>
+  );
+};
+
+export default ExploreContainer;
